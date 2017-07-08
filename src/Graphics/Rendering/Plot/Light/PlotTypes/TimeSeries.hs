ab43f1dbc4d2b6829f3fc4bb873548f7ffe03a7e
ocramz <zocca.marco@gmail.com>
2017-07-08 17:35:54 +0200

    add utils

--- src/Graphics/Rendering/Plot/Light/PlotTypes/TimeSeries.hs
+++ src/Graphics/Rendering/Plot/Light/PlotTypes/TimeSeries.hs
@@ -1,3 +1,27 @@
 module Graphics.Rendering.Plot.Light.PlotTypes.TimeSeries where
 
 import Data.Time
+import qualified Data.Text as T
+
+
+
+-- | A point in a time series
+
+data Tick =
+    D Day
+  | T TimeOfDay
+  | DT Day TimeOfDay
+  deriving (Eq, Show)
+
+data TsPoint a =
+  Tsp {
+    _tick :: Tick,
+    _value :: a
+    } deriving (Eq, Show)
+
+showTick :: Tick -> T.Text
+showTick (D d) = T.pack $ show d
+showTick (T t) = T.pack $ show t
+showTick (DT d t) = T.pack $ unwords [show d, show t]
+
+
