be3be0add6f9a4872b04b441fff8bf0222caac93
ocramz <zocca.marco@gmail.com>
2017-07-10 20:56:22 +0200

    adding quickcheck properties and tests

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,3 +1,4 @@
+{-# language FlexibleInstances, ScopedTypeVariables #-}
 module LibSpec where
 
 import Test.Hspec
@@ -12,10 +13,12 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "Lib" $ do
-    it "works" $ do
+    it "works" $ 
       True `shouldBe` True
-    -- prop "propDMat 1" $
-    --   (DiagMat2r )
+    prop "prop_matSemigroup" $
+      \(PropMatVec (m :: Mat2r Float) v ) -> prop_matSemigroup m v
+    prop "prop_multMatGroup" $
+      \(PropDiagMatVec (m :: DiagMat2r Float) v ) -> prop_multMatGroup m v
     -- prop "ourAdd is commutative" $ \x y ->
     --   ourAdd x y `shouldBe` ourAdd y x
 
@@ -23,26 +26,53 @@ spec =
 
 -- * Properties
 
--- prop_matSemi
+prop_matSemigroup ::
+  (MultiplicativeSemigroup m, LinearMap m v, Eps v) => m -> v -> Bool
+prop_matSemigroup m v = (m ## m) #> v ~= (m #> (m #> v))
 
 prop_multMatGroup :: (MatrixGroup m v, Eps v) => m -> v -> Bool
 prop_multMatGroup m v = m <\> (m #> v) ~= v
 
 
+data PropMatVec a = PropMatVec (Mat2r a) (V2r a) deriving (Eq, Show)
+instance Arbitrary (PropMatVec Float) where
+  arbitrary = PropMatVec <$> arbitrary <*> arbitrary
+
+data PropDiagMatVec a = PropDiagMatVec (DiagMat2r a) (V2r a) deriving (Eq, Show)
+instance Arbitrary (PropDiagMatVec Float) where
+  arbitrary = PropDiagMatVec <$> arbitrary <*> arbitrary
+
+
 -- * Newtypes and their Arbitrary instances
 
-newtype V2r a = V2r { v2r :: V2 a } deriving (Eq, Show)
-instance Num a => Arbitrary (V2r a) where
-  arbitrary = arbitrary
+
+-- | Float
+type V2r a = V2 a
+type Mat2r a = Mat2 a
+type DiagMat2r a = DiagMat2 a
+
+instance Arbitrary (V2r Float) where
+  arbitrary = V2 <$> arbitrary <*> arbitrary
+
+instance Arbitrary (Mat2r Float) where
+  arbitrary = Mat2 <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance Arbitrary (DiagMat2r Float) where
+  arbitrary = DMat2 <$> arbitrary <*> arbitrary
 
 
-newtype Mat2r a = M2r { m2r :: Mat2 a} deriving (Eq, Show)
-instance Arbitrary (Mat2r a) where
-  arbitrary = arbitrary
+-- | Double
+instance Arbitrary (V2r Double) where
+  arbitrary = V2 <$> arbitrary <*> arbitrary
 
-newtype DiagMat2r a = Dm2r { dm2r :: DiagMat2 a } deriving (Eq, Show)
-instance Arbitrary (DiagMat2r a) where
-  arbitrary = arbitrary
+instance Arbitrary (Mat2r Double) where
+  arbitrary = Mat2 <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance Arbitrary (DiagMat2r Double) where
+  arbitrary = DMat2 <$> arbitrary <*> arbitrary
+
+
+
 
 
 -- helpers
